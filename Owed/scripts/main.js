d7ed071305b4b57ad347a976740d15cff877cbb4
csudhtoro <32418263+csudhtoro@users.noreply.github.com>
2018-10-10 19:45:01 -0700

    Add files via upload

--- Owed/scripts/main.js
+++ Owed/scripts/main.js
@@ -1,14 +1,3 @@
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "1000px";
-}
-
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-}
-
-
 $( function() {
   $( "#datepicker" ).datepicker();
 } );
